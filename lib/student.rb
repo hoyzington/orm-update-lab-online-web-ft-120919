d1d46bc8aaf52c953fa9d366a2faa465c35f4ee8
Scott Hoisington <hoyzington@gmail.com>
2020-01-24 06:17:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,11 +38,8 @@ class Student
     student
   end
 
-  def self.new_from_db(array)
-    sql = <<-SQL
-      INSERT INTO students
-    SQL
-    DB[:conn].execute(sql)
+  def self.new_from_db(row)
+    student = self.new
   end
 
   def self.find_by_name(name)
