96cb0bbeca78b91a597af20ce27deba7e81f262a
Scott Hoisington <hoyzington@gmail.com>
2020-01-24 05:42:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,5 +11,35 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS students")
+  end
+
+  def save
+    
+  end
+
+  def self.create
+    
+  end
+
+  def self.new_from_db(array)
+    
+  end
+
+  def self.find_by_name(name)
+    
+  end
+
+  def update
+    
+  end
 
 end
